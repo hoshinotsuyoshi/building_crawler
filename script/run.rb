7c0a4453b84ffadd45466a5fa75cf653f9957db0
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2015-03-11 16:24:05 +0900

    Modify script

--- script/run.rb
+++ script/run.rb
@@ -3,6 +3,7 @@ require 'crawler'
 require 'pry'
 
 c = Crawler.new(start_at: 'http://www.officeiten.jp/addr/aa3/ha20/')
+#c = Crawler.new(start_at: 'http://www.officeiten.jp/addr/aa3/ha20/za219/')
 p = Printer.new
 
 c.visit_start_page
@@ -11,4 +12,10 @@ p.print_header(c.offices.first)
 
 c.offices.each do |o|
   p.print(o)
+end
+
+while c.visit_next_page do
+  c.offices.each do |o|
+    p.print(o)
+  end
 end
