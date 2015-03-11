71990293600a9482465bd67f96d2d0687827d347
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2015-03-11 13:59:27 +0900

    Refactor code order

--- lib/crawler.rb
+++ lib/crawler.rb
@@ -6,23 +6,23 @@ class Crawler < Mechanize
     @start_at = start_at
   end
 
+  def scheme_and_host
+    @start_at.split('/')[0, 3].join('/')
+  end
+
   def current_page
     @start_at
-  end
-
-  def visit_start_page
-    get(@start_at)
-  end
-
-  def next_page_link_element
-    (page/("//*[@id='list1']/div/a")).find{|e| e.text =~ /次のページ/ }
   end
 
   def next_page_url
     scheme_and_host + next_page_link_element.attr(:href)
   end
 
-  def scheme_and_host
-    @start_at.split('/')[0, 3].join('/')
+  def next_page_link_element
+    (page/("//*[@id='list1']/div/a")).find{|e| e.text =~ /次のページ/ }
+  end
+
+  def visit_start_page
+    get(@start_at)
   end
 end
