30052ed8e4f8e3ea7b0fb264d123fc0b506ec38f
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2015-03-11 13:42:39 +0900

    Add #next_page_link_element

--- lib/crawler.rb
+++ lib/crawler.rb
@@ -2,6 +2,7 @@ require 'mechanize'
 
 class Crawler < Mechanize
   def initialize(start_at: nil)
+    super
     @start_at = start_at
   end
 
@@ -11,5 +12,12 @@ class Crawler < Mechanize
 
   def visit_start_page
     get(@start_at)
+  end
+
+  def next_page_link_element
+    (page/("//*[@id='list1']/div/a")).find{|e| e.text =~ /次のページ/ }
+  end
+
+  def next_page_url
   end
 end
--- spec/lib/crawler_spec.rb
+++ spec/lib/crawler_spec.rb
@@ -14,6 +14,26 @@ describe Crawler do
     end
   end
 
+  describe '#next_page_link_element' do
+    it do
+      fixture_path = PROJECT_ROOT + 'spec/fixtures/ha20.html'
+      WebMock.stub_request(
+        :get,
+        'http://www.officeiten.jp/addr/aa3/ha20/'
+      ).to_return(
+        body: fixture_path.read,
+        headers: { content_type: 'text/html' }
+      )
+
+      c = Crawler.new(start_at: 'http://www.officeiten.jp/addr/aa3/ha20/')
+      c.visit_start_page
+      link_element = c.next_page_link_element
+
+      expect(link_element).to be_a Nokogiri::XML::Element
+      expect(link_element.attr(:href)).to eq '/addr/aa3/ha20/za2/'
+    end
+  end
+
   describe '#visit_start_page' do
     it do
       c = Crawler.new(start_at: 'http://www.officeiten.jp/addr/aa3/ha20/')
